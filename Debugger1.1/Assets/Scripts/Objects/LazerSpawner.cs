eff2a69db5ec50b223f9da69aff4d12a2c927999
Jonathan Gyurkovics <jdgyurkovics@fullsail.edu>
2015-08-11 15:10:34 -0400

    Finished setting up the Straw Man.

---
+++ Debugger1.1/Assets/Scripts/Objects/LazerSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LazerSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
