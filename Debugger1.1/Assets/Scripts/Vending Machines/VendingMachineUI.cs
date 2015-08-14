7d4615a3c02b847afb5c77e724387f2650b50352
Darryl Warner <dvwarner@fullsail.edu>
2015-08-14 15:35:07 -0400

    Vending Machine menu templates (work in progress)

---
+++ Debugger1.1/Assets/Scripts/Vending Machines/VendingMachineUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VendingMachineUI : MonoBehaviour {
+
+	[SerializeField]
+	int price = 0;
+	[SerializeField]
+	Statistics buyer = null;
+
+	public void ConfirmPurchase() {
+		if(CheckPrices())
+			Application.LoadLevelAsync ("ConfirmPurchase");
+	}
+
+	// returns true if the player can afford to purchase this item; returns false otherwise
+	bool CheckPrices() {
+		return true;
+	}
+}
