f98a308a0ece0d4d7f65bb1a5f0516121eeafddc
Jonathan Gyurkovics <jdgyurkovics@fullsail.edu>
2015-08-14 15:27:10 -0400

    Wrote the Statistics class for all objects that will have stats. Added a player and gave him a movement controller and a derived version of the statistics class.

--- Debugger1.1/Assets/Scripts/Player/Player.cs
+++ Debugger1.1/Assets/Scripts/Player/Player.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : Statistics {
+
+	[SerializeField]
+	int money = 0;
+	[SerializeField]
+	int exp = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+		currHealth = maxHealth = initialHealth + healthPerEndurance * endurance;
+		damage = initialDamage + damagePerStrength * strength;
+		critChance = initialCrit + critPerLuck * luck;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (currHealth == 0) {
+		}
 	}
+
+	public int EXP { get; set; }
+	public int Money { get; set; }
 }
--- Debugger1.1/Assets/Scripts/Player/PlayerController.cs
+++ Debugger1.1/Assets/Scripts/Player/PlayerController.cs
@@ -3,13 +3,57 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	[SerializeField]
+	Transform PlayerSprite = null;
+	Statistics Player = null;
+
+	[SerializeField]
+	Vector3 moveDir = Vector3.zero;
+	[SerializeField]
+	float playerVelocity = 0.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+		Player = GetComponentInChildren<Statistics> ();
+		PlayerSprite = GameObject.FindGameObjectWithTag ("Player").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		Vector3 direction = Camera.main.WorldToScreenPoint (transform.position) - Input.mousePosition;
+		direction.Normalize();
+		float rot = (Mathf.Atan2(-direction.y, direction.x) * 180 / Mathf.PI) - 90;
+		PlayerSprite.rotation = Quaternion.Euler (0, rot, 0);
+	}
+
+	void FixedUpdate() {
+		//moveDir = Vector3.zero;
+		playerVelocity = Player.Velocity;
+		moveDir.x = Input.GetAxisRaw ("Horizontal");
+		moveDir.z = Input.GetAxisRaw ("Vertical");
+
+		if (moveDir.magnitude > 0.0f) {
+			if (Player.Velocity < Player.MaxVelocity) {
+				Player.Velocity += Player.Acceleration * Time.deltaTime;
+
+				if (Player.Velocity > Player.MaxVelocity)
+					Player.Velocity = Player.MaxVelocity;
+			}
+		}
+		if (moveDir.magnitude == 0.0f) {
+			if(Player.Velocity > 0.0f){
+				Player.Velocity -= Player.Acceleration * Time.deltaTime;
+
+				if(Player.Velocity < 0.0f)
+					Player.Velocity = 0.0f;
+			}
+		}
+
+		if (moveDir.magnitude > 1.0f)
+			moveDir.Normalize ();
+
+		moveDir *= Player.Velocity;
+
+		gameObject.GetComponent<Rigidbody> ().velocity = moveDir;
 	}
 }
