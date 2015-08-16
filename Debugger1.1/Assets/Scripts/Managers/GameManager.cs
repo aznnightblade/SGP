b4e4f349e5b16a83476610d134f5589334f88e93
Brandon Rodriguez <bwrodriguez@fullsail.edu>
2015-08-16 16:43:00 -0400

    gameManger start

--- Debugger1.1/Assets/Scripts/Managers/GameManager.cs
+++ Debugger1.1/Assets/Scripts/Managers/GameManager.cs
@@ -3,6 +3,21 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	public static GameManager instance;
+	private int credits;
+	private int xp;
+	void Awake(){ 
+
+		instance = this;
+	}
+
+	public void AddXp(int _numXp){
+		xp = xp + _numXp;
+	}
+	public void AddCredits(int _numCredits){
+		credits = credits + _numCredits;
+	}
+
 	// Use this for initialization
 	void Start () {
 	
