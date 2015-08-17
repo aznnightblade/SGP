a3b04664b37ce74cbce67d6998148b771a2b1388
Darryl Warner <dvwarner@fullsail.edu>
2015-08-17 18:51:14 -0400

    Working on Companion class.

---
+++ Debugger1.1/Assets/Scripts/Companions/Companion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Companion : Statistics {
+
+	enum StatToBuff {NONE, STRENGTH, ENDURANCE, AGILITY, DEXTERITY, INTELLIGENCE, LUCK};
+	bool playerInRange = false;
+
+	[SerializeField]
+	StatToBuff statToBuff = StatToBuff.NONE; // which stat should be buffed
+	[SerializeField]
+	int buffAmount = 0; // amount to buff that stat by
+	[SerializeField]
+	Player player = null;
+	[SerializeField]
+	int shieldBuff = 0; // amount to increase player shield level by
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 playerPos = player.GetComponent<Transform> ().position;
+	}
+}
--- Debugger1.1/Assets/Scripts/Pickups/HealthPickup.cs
+++ Debugger1.1/Assets/Scripts/Pickups/HealthPickup.cs
@@ -3,8 +3,15 @@ using System.Collections;
 
 public class HealthPickup : MonoBehaviour {
 
+	public SoundManager sound;
+
+	void Start() {
+		sound = GameObject.FindGameObjectWithTag ("SoundManager").GetComponent<SoundManager> ();
+	}
+
 	void OnTriggerEnter(Collider col) {
 		if (col.gameObject.tag == "Player") {
+			sound.MiscSoundeffects[0].Play();
 			// Restores 15% of player's max health
 			int healthRestored = (int)(col.gameObject.GetComponent<Statistics>().MaxHealth * 0.15f);
 			col.gameObject.GetComponent<Statistics>().CurrHealth += healthRestored;
