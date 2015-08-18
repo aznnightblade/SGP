5a439358e01bbf67b2623e41686a63f48ff4adcf
Darryl Warner <dvwarner@fullsail.edu>
2015-08-18 15:44:29 -0400

    Completed Companion class.  Needs to be tested and checked off.

--- Debugger1.1/Assets/Scripts/Companions/Companion.cs
+++ Debugger1.1/Assets/Scripts/Companions/Companion.cs
@@ -4,37 +4,40 @@ using System.Collections;
 public class Companion : Statistics {
 
 	enum StatToBuff {NONE, STRENGTH, ENDURANCE, AGILITY, DEXTERITY, INTELLIGENCE, LUCK};
-	bool playerInRange = false;
+	bool buffApplied = false;
+	Player player = null;
+	NavMeshAgent agent = null;
 
 	[SerializeField]
 	StatToBuff statToBuff = StatToBuff.NONE; // which stat should be buffed
 	[SerializeField]
 	int buffAmount = 0; // amount to buff that stat by
 	[SerializeField]
-	Player player = null;
-	[SerializeField]
 	int shieldBuff = 0; // amount to increase player shield level by
-	[SerializeField]
-	float maxBuffRadius = 0.0f;
 
 	// Use this for initialization
 	void Start () {
+		agent = gameObject.GetComponent<NavMeshAgent> ();
 		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player> ();
+		agent.destination = player.GetComponent<Transform> ().position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 playerPos = player.GetComponent<Transform> ().position;
-		if (Vector3.Distance (playerPos, transform.position) <= maxBuffRadius) {
-			playerInRange = true;
-		} else {
-			playerInRange = false;
-		}
+		agent.destination = player.GetComponent<Transform> ().position;
 
+		// apply stat buff if companion is alive, remove it when companion dies
+		if (currHealth <= 0) {
+			RemoveBuffs ();
+			DestroyObject ();
+		} else {
+			AddBuffs();
+		}
 	}
 
 	void AddBuffs() {
-		if (playerInRange) {
+		// Only apply stat buff once
+		if(!buffApplied){
 			switch (statToBuff) {
 			case StatToBuff.STRENGTH:
 				player.Strength += buffAmount;
@@ -57,6 +60,42 @@ public class Companion : Statistics {
 			case StatToBuff.NONE:
 				break;
 			}
+			
+			player.Shield += shieldBuff;
 		}
+
+		buffApplied = true;
+	}
+
+	void RemoveBuffs() {
+		// Only do this if player has a stat buff
+		if(buffApplied) {
+			switch (statToBuff) {
+			case StatToBuff.STRENGTH:
+				player.Strength -= buffAmount;
+				break;
+			case StatToBuff.ENDURANCE:
+				player.Endurance -= buffAmount;
+				break;
+			case StatToBuff.AGILITY:
+				player.Agility -= buffAmount;
+				break;
+			case StatToBuff.DEXTERITY:
+				player.Dexterity -= buffAmount;
+				break;
+			case StatToBuff.INTELLIGENCE:
+				player.Intelligence -= buffAmount;
+				break;
+			case StatToBuff.LUCK:
+				player.Luck -= buffAmount;
+				break;
+			case StatToBuff.NONE:
+				break;
+			}
+
+			player.Shield -= shieldBuff;
+		}
+
+		buffApplied = false;
 	}
 }
