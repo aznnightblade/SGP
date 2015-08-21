5e89a594d7217b97186bc349e1062cdf4deb049f
Michael Lin <AznNightblade@gmail.com>
2015-08-21 18:42:53 -0400

    more sounds added

--- Debugger1.1/Assets/Scripts/Objects/Corruption.cs
+++ Debugger1.1/Assets/Scripts/Objects/Corruption.cs
@@ -2,9 +2,17 @@
 using System.Collections;
 
 public class Corruption : Statistics {
+    public SoundManager sounds;
 
+    void Start()
+    {
+        sounds = GameObject.FindGameObjectWithTag("SoundManager").GetComponent<SoundManager>();
+    }
 	void Update(){
-		if (gameObject.GetComponent<Statistics> ().CurrHealth <= 0)
-			Destroy (gameObject);
+        if (gameObject.GetComponent<Statistics>().CurrHealth <= 0)
+        {
+            sounds.MiscSoundeffects[4].Play();
+            Destroy(gameObject);
+        }
 	}
 }
