7d459ef0abbb7305a8fa2395471b8e7aef35b53b
Michael Lin <AznNightblade@gmail.com>
2015-08-23 16:20:25 -0400

    added npc in hubworld

---
+++ Debugger1.1/Assets/Scripts/npc1movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class npc1movement : MonoBehaviour {
+    [SerializeField]
+	Transform target = null;
+
+    public bool active = false;
+    Random rnd = new Random();
+
+    private NavMeshAgent agent = null;
+    [SerializeField]
+    Transform[] Waypoints = null;
+    Vector3 destination = Vector3.zero;
+	// Use this for initialization
+	void Start () {
+
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        agent.destination = Waypoints[Random.Range(0, Waypoints.Length)].transform.position;
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+       if (gameObject.GetComponent<NavMeshAgent>().remainingDistance <= 3)
+       {
+          agent.destination = Waypoints[Random.Range(0, Waypoints.Length)].transform.position;
+       }
+    }
+}
