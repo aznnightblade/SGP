4369c8111e2aff7166f4eb058bd7360cb6996e10
Darryl Warner <dvwarner@fullsail.edu>
2015-08-24 15:19:31 -0400

    Made options menu.  Functions for audio and key config have yet to be implemented.

---
+++ Debugger1.1/Assets/Scripts/Options/AdjustVolume.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class AdjustVolume : MonoBehaviour {
+
+	public void SFXVolumeUp() {
+
+	}
+
+	public void SFXVolumeDown() {
+
+	}
+
+	public void MusicVolumeUp() {
+
+	}
+
+	public void MusicVolumeDown() {
+
+	}
+}
--- Debugger1.1/Assets/Scripts/Player/ColorSwitch.cs
+++ Debugger1.1/Assets/Scripts/Player/ColorSwitch.cs
@@ -4,19 +4,34 @@ using System.Collections;
 public class ColorSwitch : MonoBehaviour {
 
 	Player player = null;
+	//InputType colorSwapKey = null;
 
 	// Use this for initialization
 	void Start () {
 		player = gameObject.GetComponentInChildren<Player> ();
+		//colorSwapKey = InputManager.instance.keyboardInputs [7];
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown ("ColorSwitch"))
+		if (InputManager.instance.GetButtonDown("ColorSwap") && player.HasDLLs)
 			SwitchColors ();
 	}
 
 	void SwitchColors() {
-
+		switch (player.CurrWeapon.CurrColor) {
+		case DLLColor.Color.RED:
+			player.CurrWeapon.CurrColor = DLLColor.Color.GREEN;
+			break;
+		case DLLColor.Color.GREEN:
+			player.CurrWeapon.CurrColor = DLLColor.Color.BLUE;
+			break;
+		case DLLColor.Color.BLUE:
+			player.CurrWeapon.CurrColor = DLLColor.Color.RED;
+			break;
+		case DLLColor.Color.NEUTRAL:
+			player.CurrWeapon.CurrColor = DLLColor.Color.RED;
+			break;
+		}
 	}
 }
