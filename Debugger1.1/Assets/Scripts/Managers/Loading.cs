c640ab95805b05ff0a95098da9a14482a5a61167
Brandon Rodriguez <bwrodriguez@fullsail.edu>
2015-08-25 00:23:11 -0400

    Tutorial Ready for Story

---
+++ Debugger1.1/Assets/Scripts/Managers/Loading.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loading: MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		GameManager.instance.LoadScene();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Debugger1.1/Assets/Scripts/Objects/CompanionCube.cs
+++ Debugger1.1/Assets/Scripts/Objects/CompanionCube.cs
@@ -6,10 +6,30 @@ public class CompanionCube : MonoBehaviour {
 	// Use this for initialization
 	public int Next;
 	public string HubWorld;
-
-	void OnCollisionEnter(Collision col)
+	bool activateSwitch;
+	void OnTriggerEnter(Collider col)
 	{
+		if (col.tag == "Player" )
+		{
+			activateSwitch = true;
+		}
+	}
+	
+	void OnTriggerExit(Collider col)
+	{
+		if (col.tag== "Player")
+		{
+			activateSwitch = false;
+		}
+	}
 
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Submit") && activateSwitch == true) {
 			if (GameManager.indexLevel < Next) {
 				GameManager.levelComplete (Next);
 			}
@@ -18,12 +38,5 @@ public class CompanionCube : MonoBehaviour {
 			GameManager.back = true;
 
 	}
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+}
 }
