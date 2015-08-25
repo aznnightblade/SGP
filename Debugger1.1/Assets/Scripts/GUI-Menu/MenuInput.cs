df9b2e1edfa43360fa9f2448741ef01c47c623f2
Jonathan Gyurkovics <jdgyurkovics@fullsail.edu>
2015-08-25 16:08:34 -0400

    Addd the Menu Input script so you can navigate through menus with the keyboard and controller.

--- Debugger1.1/Assets/Scripts/GUI-Menu/MenuInput.cs
+++ Debugger1.1/Assets/Scripts/GUI-Menu/MenuInput.cs
@@ -1,19 +1,87 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class MenuInput : MonoBehaviour {
+public class MenuInput : EventSystem {
+	[SerializeField]
+	float moveDelay = 0.5f;
+	float moveDelayTimer = 0.0f;
+	float lastTime = 0.0f;
 
-    StandaloneInputModule input = null;
+    [SerializeField]
+	button[] Buttons = null;
+	GameObject selectedButton = null;
 
-	// Use this for initialization
-	void Start () {
-        input = gameObject.GetComponent<StandaloneInputModule>();
+	int currRow = 0;
+	int currColumn = 0;
 
+	override protected void Start () {
+		lastTime = Time.timeSinceLevelLoad;
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	override protected void Update () {
+		float input = 0.0f;
+
+		if (moveDelayTimer <= 0.0f) {
+			if (selectedButton == null) {
+				selectedButton = Buttons [currColumn].buttons [currRow];
+
+				Buttons [currColumn].buttons [currRow].GetComponent<Button> ().Select ();
+				moveDelayTimer = moveDelay;
+			} else {
+				if ((input = InputManager.instance.GetAxisRaw ("Horizontal")) != 0.0f) {
+					if (Buttons[currColumn].buttons [currRow].tag == "Slider") {
+						if (input < 0.0f) {
+							Buttons[currColumn].buttons [currRow].GetComponent<Slider> ().value -= 1;
+						} else if (input > 0.0f) {
+							Buttons[currColumn].buttons [currRow].GetComponent<Slider> ().value += 1;
+						}
+
+						moveDelayTimer = moveDelay * 0.2f;
+					} else {
+						if (input < 0.0f && currColumn != 0) {
+							currColumn--;
+						} else if (input > 0.0f && currColumn != Buttons.Length - 1) {
+							currColumn++;
+						}
+
+						if (currRow >= Buttons [currColumn].buttons.Length - 1)
+							currRow = Buttons [currColumn].buttons.Length - 1;
+
+						Buttons [currColumn].buttons [currRow].GetComponent<Button> ().Select ();
+						selectedButton = Buttons [currColumn].buttons [currRow];
+						moveDelayTimer = moveDelay;
+					}
+				} else if ((input = InputManager.instance.GetAxisRaw ("Vertical")) != 0.0f) {
+					if (input < 0.0f && currRow != Buttons [currColumn].buttons.Length - 1) {
+						currRow++;
+					} else if (input > 0.0f && currRow != 0) {
+						currRow--;
+					}
+
+					if (Buttons [currColumn].buttons [currRow].tag != "Slider")
+						Buttons [currColumn].buttons [currRow].GetComponent<Button> ().Select ();
+					selectedButton = Buttons [currColumn].buttons [currRow];
+					moveDelayTimer = moveDelay;
+				} else if ((input = InputManager.instance.GetAxisRaw ("Submit")) != 0.0f) {
+					if (selectedButton != null)
+						ExecuteEvents.Execute (selectedButton, null, ExecuteEvents.submitHandler);
+				}
+			}
+		} else {
+			float currTime = Time.timeSinceLevelLoad;
+			moveDelayTimer -= currTime - lastTime;
+			lastTime = currTime;
+
+			if (moveDelayTimer <= 0.0f)
+				moveDelayTimer = 0.0f;
+		}
 	}
+}
+
+[System.Serializable]
+public struct button {
+	public GameObject[] buttons;
 }
