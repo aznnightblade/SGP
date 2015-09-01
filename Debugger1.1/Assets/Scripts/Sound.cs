b744870daf802ef1be0c28a5bc285858510f11ce
Michael Lin <AznNightblade@gmail.com>
2015-09-01 19:07:45 -0400

    more sounds update

---
+++ Debugger1.1/Assets/Scripts/Sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        for (int i = 0; i < SoundManager.instance.Music.Count; i++)
+        {
+            SoundManager.instance.Music[i].Stop();
+        }
+        for (int i = 0; i < SoundManager.instance.BossMusic.Count; i++)
+        {
+            SoundManager.instance.BossMusic[i].Stop();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
