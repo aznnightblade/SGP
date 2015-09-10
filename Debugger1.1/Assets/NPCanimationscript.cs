33f6d2b3474ce04beea158b5217536bc1701d9c4
Michael Lin <AznNightblade@gmail.com>
2015-09-10 19:43:05 -0400

    joe's animation

---
+++ Debugger1.1/Assets/NPCanimationscript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCanimationscript : MonoBehaviour {
+    Animator interaction;
+	// Use this for initialization
+	void Start () {
+        interaction = GetComponentInChildren<Animator>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            interaction.SetBool("Talking", true);
+        }
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            interaction.SetBool("Talking", false);
+        }
+    }
+
+}
