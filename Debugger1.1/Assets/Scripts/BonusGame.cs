0ace3e243a2fd3b4fbda63284f28a97caa581c44
Brandon Rodriguez <bwrodriguez@fullsail.edu>
2015-09-13 19:55:38 -0400

    bonus level

---
+++ Debugger1.1/Assets/Scripts/BonusGame.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusGame : MonoBehaviour {
+	public int credits;
+	public GameObject player;
+	public GameObject[] show;
+	public GameObject five;
+	public GameObject one;
+	// Use this for initialization
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Enemy" )
+		{
+			player.GetComponentInChildren<Player>().Money += credits;
+
+			switch(credits)
+			{
+			case 1:
+			{
+				show[0].SetActive(true);
+				show[1].SetActive(false);
+				show[2].SetActive(false);
+				break;
+			}
+			case 5:
+			{
+				show[0].SetActive(false);
+				show[1].SetActive(true);
+				show[2].SetActive(false);
+				break;
+			}
+			case 25:
+			{
+				show[0].SetActive(false);
+				show[1].SetActive(false);
+				show[2].SetActive(true);
+				break;
+			}
+		}
+	}
+}
+}
