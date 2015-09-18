631f4c599377e8b1e354f50f1a54cdf383939d54
Brandon Rodriguez <bwrodriguez@fullsail.edu>
2015-09-18 11:25:42 -0400

    final tweaks to hubworld

--- Debugger1.1/Assets/IsItSeven.cs
+++ Debugger1.1/Assets/IsItSeven.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class IsItSeven : MonoBehaviour {
 	public GameObject door;
+	public GameObject pressEnter;
 	// Use this for initialization
 	void Start () {
 	
@@ -12,5 +13,9 @@ public class IsItSeven : MonoBehaviour {
 	void Update () {
 	if (GameManager.indexLevel == 7)
 			door.SetActive (false);
+
+	if (GameManager.indexLevel > 1)
+			pressEnter.SetActive (false);
 	}
+
 }
